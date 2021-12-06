76def80e232b7d73fc3c6c8f86badeed9769866b
Sebastian Morkisch <semox78@gmail.com>
2021-12-06 10:38:46 +0100

    listening to two different Observables and consoling.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 import { ISTandardMessageInterface } from './core/model/istandard-message-interface';
-import { OneTypedMessageInterface } from './core/model/one-typed-message-interface';
+import { OneTypedMessage, OneTypedMessageInterface } from './core/model/one-typed-message-interface';
+import { Point } from "geojson";
+import { TwoTypedMessage, TwoTypedMessageInterface } from './core/model/two-typed-message-interface';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +14,30 @@ export class AppComponent implements OnInit {
   
   title = 'store';
 
-  watch!: ISTandardMessageInterface<OneTypedMessageInterface>;
+  stdMess!: ISTandardMessageInterface<OneTypedMessageInterface>;
+  sepMess!: ISTandardMessageInterface<TwoTypedMessageInterface>;
 
-  
+  subjectStdMess = new BehaviorSubject(this.stdMess);
+  subjectSepMess = new BehaviorSubject(this.sepMess);
+
   constructor() {}
   
   ngOnInit(): void {
-    
-    console.log("Heloo");
+    this.subjectStdMess.next({'type': 'otm', 'message': [new OneTypedMessage("1", "ein Poi", {"type": "Point", "coordinates": [7.0069, 51.1623]}, new Date())]});
+
+
+    this.subjectStdMess.subscribe(message => {
+      console.log('Access some Message Object at constructor -->', message.message[0]);
+    });
+
+    this.subjectSepMess.next({'type': 'sep', 'message': [new TwoTypedMessage("1", "Olaf", "Scholz")]});
+
+
+    this.subjectSepMess.subscribe(message => {
+      console.log('Access some Message Object at constructor -->', message.message[0]);
+    });
+
+    console.log("Helloo");
   }
 
 }
