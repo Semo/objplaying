b99a80327e931e952e7e4d591d7924e2b79612bd
Sebastian Morkisch <semox78@gmail.com>
2021-12-06 10:39:35 +0100

    exporting interfaces into usable messages, bc they were missing.

--- src/app/core/model/one-typed-message-interface.ts
+++ src/app/core/model/one-typed-message-interface.ts
@@ -8,3 +8,18 @@ export interface OneTypedMessageInterface {
     dattime: Date;
 
 }
+
+export class OneTypedMessage implements OneTypedMessageInterface {
+    id: string;
+    poi_info: string;
+    geoloc: Point;
+    dattime: Date;
+
+    constructor (id: string, poi_info:string, geoloc: Point, dattime: Date) {
+        this.id = id;
+        this.poi_info = poi_info;
+        this.geoloc = geoloc;
+        this.dattime = dattime;
+    }
+    
+}
--- src/app/core/model/two-typed-message-interface.ts
+++ src/app/core/model/two-typed-message-interface.ts
@@ -5,3 +5,16 @@ export interface TwoTypedMessageInterface {
     person_lastname: string
 
 }
+
+export class TwoTypedMessage implements TwoTypedMessageInterface {
+    id: string;
+    person_surname: string;
+    person_lastname: string;
+
+    constructor(id: string, person_surname: string, person_lastname:string) {
+        this.id = id;
+        this.person_surname = person_surname;
+        this.person_lastname = person_lastname;
+    }
+
+}
